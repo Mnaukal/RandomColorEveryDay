fd0c2796b91ccbcdf299005f7e598c1444630365
Mnaukal <michal.topfer@gmail.com>
2015-08-20 20:08:00 +0200

    Moved php before body, better fetching mysql results

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
         -->
     </head>
-    <body style="background: 
+    
 <?php
 /*** mysql data ***/
 $hostname = 'localhost';
@@ -17,19 +17,21 @@ $dbname = 'randomcoloroftheday';
 $username = 'randomcoloruser';
 $password = 'colorpassword123';
 
+$color = "#";
+
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     /*** echo a message saying we have connected ***/
-    
+
     /*** The SQL SELECT statement ***/
     $sql = "SELECT * FROM colors
             ORDER BY ID DESC
             LIMIT 1;";
-    foreach ($dbh->query($sql) as $row)
-    {
-        print '#'.$row['color'];
-    }
+    $stm = $dbh->query($sql);
     
+    $result = $stm->fetch(PDO::FETCH_OBJ);
+    
+    $color = "#".$result->color;
     
     /*** close the database connection ***/
     $dbh = null; 
@@ -38,6 +40,15 @@ catch(PDOException $e)
 {
     echo $e->getMessage();
 }
-?>">
+?>   
+   
+    <body style="background:<?php print($color) ?>">
+    <div id="title">
+        <h2>Today's random color is </h2>
+    <h1><?php 
+    print($color);
+?>
+        </h1></div>
+    
     </body>
 </html>
