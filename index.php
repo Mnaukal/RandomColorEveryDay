175941599f0a933a0c7060e07bfd4456dce08a84
Mnaukal <michal.topfer@gmail.com>
2015-08-21 09:46:50 +0200

    Added stylesheet, moved PHP before head

--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <!doctype html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Random Color of the Day</title>
-        <!--
-        <link rel="stylesheet" href="style.css">        
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-        -->
-    </head>
-    
-<?php
+<html>    
+    <?php
 /*** mysql data ***/
 $hostname = 'localhost';
 $dbname = 'randomcoloroftheday';
@@ -28,11 +18,11 @@ try {
             ORDER BY ID DESC
             LIMIT 1;";
     $stm = $dbh->query($sql);
-    
+
     $result = $stm->fetch(PDO::FETCH_OBJ);
-    
+
     $color = "#".$result->color;
-    
+
     /*** close the database connection ***/
     $dbh = null; 
 }
@@ -40,15 +30,24 @@ catch(PDOException $e)
 {
     echo $e->getMessage();
 }
-?>   
-   
+    ?>   
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <title><?php print($color); ?> - Random Color of the Day</title>
+        <link rel="stylesheet" href="style.css"> 
+
+        <!--       
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+-->
+    </head>
+
     <body style="background:<?php print($color) ?>">
-    <div id="title">
-        <h2>Today's random color is </h2>
-    <h1><?php 
-    print($color);
-?>
-        </h1></div>
-    
+        <div id="title">
+            <h2>Today's random color is </h2>
+            <h1><?php print($color); ?></h1>
+        </div>
+
     </body>
 </html>
