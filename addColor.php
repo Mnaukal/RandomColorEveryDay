2d4fcb5c6db4c671912a8f5f3dba13c406e66ab2
Mnaukal <michal.topfer@gmail.com>
2015-08-23 21:01:57 +0200

    Generate color.png when adding new color (daily)

--- addColor.php
+++ addColor.php
@@ -8,7 +8,7 @@ $password = 'colorpassword123';
 try {
     $dbh = new PDO("mysql:host=$hostname;dbname=$dbname", $username, $password);
     /*** echo a message saying we have connected ***/
-    echo 'Connected to database<br>';
+    //echo 'Connected to database<br>';
     
     $color = sprintf ("%06x", mt_rand(0, 0xffffff));
     
@@ -18,7 +18,20 @@ try {
     $count = $dbh->exec("INSERT INTO colors(color, date) VALUES ('$color', '$date')");
 
     /*** echo the number of affected rows ***/
-    echo $count;
+    //echo $count;
+    
+    header("Content-type: image/png");
+    $im     = imagecreatefrompng("color.png");
+    
+    $r = hexdec(substr($color, 0, 2));
+    $g = hexdec(substr($color, 2, 2));
+    $b = hexdec(substr($color, 4, 2));
+    
+    $col = imagecolorallocate($im, $r, $g, $b);
+    imagefill($im, 0, 0, $col);
+    imagepng($im, "color.png");
+    imagedestroy($im);
+    
 
     /*** close the database connection ***/
     $dbh = null;
