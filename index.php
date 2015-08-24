fdc4f5ed59925789ba2bf8f5e2ba8f640326de72
Mnaukal <michal.topfer@gmail.com>
2015-08-24 12:55:27 +0200

    Analogous and complementary colors

--- index.php
+++ index.php
@@ -1,6 +1,50 @@
 <!doctype html>
 <html>    
     <?php
+function clamp($value)
+{
+    if($value >= 360)
+        $value -= 360;
+    if($value < 0)
+        $value += 360;
+    return $value;
+}
+
+function rotateHue($amount, $iH, $iS, $iV)
+{
+    $H2 = clamp($iH + $amount);
+    
+    $dS = $iS/100.0; // Saturation: 0.0-1.0
+        $dV = $iV/100.0; // Lightness:  0.0-1.0
+        $dC = $dV*$dS;   // Chroma:     0.0-1.0
+        $dH = $H2/60.0;  // H-Prime:    0.0-6.0
+        $dT = $dH;       // Temp variable
+        while($dT >= 2.0) $dT -= 2.0; // php modulus does not work with float
+        $dX = $dC*(1-abs($dT-1));     // as used in the Wikipedia link
+        switch($dH) {
+            case($dH >= 0.0 && $dH < 1.0):
+                $dR = $dC; $dG = $dX; $dB = 0.0; break;
+            case($dH >= 1.0 && $dH < 2.0):
+                $dR = $dX; $dG = $dC; $dB = 0.0; break;
+            case($dH >= 2.0 && $dH < 3.0):
+                $dR = 0.0; $dG = $dC; $dB = $dX; break;
+            case($dH >= 3.0 && $dH < 4.0):
+                $dR = 0.0; $dG = $dX; $dB = $dC; break;
+            case($dH >= 4.0 && $dH < 5.0):
+                $dR = $dX; $dG = 0.0; $dB = $dC; break;
+            case($dH >= 5.0 && $dH < 6.0):
+                $dR = $dC; $dG = 0.0; $dB = $dX; break;
+            default:
+                $dR = 0.0; $dG = 0.0; $dB = 0.0; break;
+        }
+        $dM  = $dV - $dC;
+        $dR += $dM; $dG += $dM; $dB += $dM;
+        $dR *= 255; $dG *= 255; $dB *= 255;
+    
+    return "#".sprintf("%02X", round($dR)).sprintf("%02X", round($dG)).sprintf("%02X", round($dB));
+}
+
+
 /*** mysql data ***/
 $hostname = 'localhost';
 $dbname = 'randomcoloroftheday';
@@ -381,5 +425,69 @@ catch(PDOException $e)
             </div>
     </div>
 
+        <div class="scheme" id="analogous">
+            <h3>Analogous</h3>
+            <div class="color" style="background: <?php echo(rotateHue(-30, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(-30, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(-20, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(-20, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(-10, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(-10, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(0, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(0, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(10, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(10, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(20, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(20, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(30, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(30, $H, $S, $V));
+                ?>
+            </div>
+        </div>
+       
+       
+        <div class="scheme" id="complementary">
+            <h3>Complementary</h3>
+            <div class="color" style="background: <?php echo(rotateHue(150, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(150, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(160, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(160, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(170, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(170, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(180, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(180, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(190, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(190, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(200, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(200, $H, $S, $V));
+                ?>
+            </div>
+            <div class="color" style="background: <?php echo(rotateHue(210, $H, $S, $V)); ?>">
+                <?php echo(rotateHue(210, $H, $S, $V));
+                ?>
+            </div>
+        </div>
 </body>
 </html>
